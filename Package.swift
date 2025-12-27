bbe16d20bf9f470d63e700771c2bdbc36c2fb72e
Uğur hamzaoğlu <ugur.hmz52@gmail.com>
2025-12-27 21:52:03 +0300

    Package.swift edited.

--- Package.swift
+++ Package.swift
@@ -5,17 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "CoreNetworking",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CoreNetworking",
             targets: ["CoreNetworking"]),
     ],
+    dependencies: [
+        //
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CoreNetworking"),
+            name: "CoreNetworking",
+            dependencies:[]),
         .testTarget(
             name: "CoreNetworkingTests",
             dependencies: ["CoreNetworking"]

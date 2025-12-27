eb023c66ea5fdf5039d3fc4a8a728bbabb9109a4
Uğur hamzaoğlu <ugur.hmz52@gmail.com>
2025-12-27 22:00:53 +0300

    EmptyResponse & HTTPMethod has been created.

---
+++ Sources/CoreNetworking/Core/EmptyResponse.swift
@@ -0,0 +1,12 @@
+//
+//  EmptyResponse.swift
+//  CoreNetworking
+//
+//  Created by rico on 27.12.2025.
+//
+
+import Foundation
+
+public struct EmptyResponse: Decodable {
+    public init() {}
+}
---
+++ Sources/CoreNetworking/Core/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  CoreNetworking
+//
+//  Created by rico on 27.12.2025.
+//
+
+import Foundation
+
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
